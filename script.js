ad7c39a81583bb21e7816b0071f76db1465b28bc
Michaela <botka@email.cz>
2023-09-11 10:39:18 +0200

    malé změny formátování

--- script.js
+++ script.js
@@ -1,97 +1,90 @@
-//////////////základní proměnné
-
+//////////////MAIN VARIABLES
 var totalScore, actualScore, activePlayer, dice, playGame;
+newGame();
+zero();
 
-novaHra();
-nulovani();
 
+/////////////BUTTON "HOD KOSTKOU" - button dice
+document.querySelector(".btn5").addEventListener("click", dice);
 
-/////////////tlačítko Hod kostkou
+function dice() {
+    if (playGame) {
+        //  changing dice according to random number
+        var dice = document.querySelector(".dice");
+        var randomNumber = Math.ceil(Math.random() * 6);
+        var dicePicture = document.querySelector(".diceImage");
+        dice.style.display = "block";
+        console.log(dicePicture.src = "img/" + randomNumber + ".jpg");
 
-document.querySelector(".btn5").addEventListener("click",hod);
-
-function hod(){
-    if(playGame){
-//  měníme kostku dle náhodného čísla
-    var dice = document.querySelector(".dice");
-    var nahCislo = Math.ceil(Math.random()*6);
-    var nahKostka = document.querySelector(".diceImage");  
-    dice.style.display = "block";
-    console.log(nahKostka.src = "img/" + nahCislo + ".jpg");
-    
-    if(nahCislo !== 1){
-        //  Nasčítávání čísel z kostky do round score
-        roundScore = roundScore + nahCislo;
-        document.querySelector(".actualScore"+activePlayer).textContent = roundScore;
-    } 
-    else {
-        //  Pokud se hodí 1, tak vynuluj actual score a hraje 2.hráč
-        zmenaHrace();
+        if (randomNumber !== 1) {
+            //  adding numbers from dice to round score
+            roundScore = roundScore + randomNumber;
+            document.querySelector(".actualScore" + activePlayer).textContent = roundScore;
+        }
+        else {
+            //  if random nuber is 1, actual score change to null and 2nd player is playinng
+            changingPlayer();
+        };
     };
-         };
 };
 
-//////////tlačítko Přiřad score
 
-document.querySelector(".btn6").addEventListener("click",prirad);
+//////////BUTTON "PŘIŘAĎ SCORE" - button adding score
+document.querySelector(".btn6").addEventListener("click", addingScore);
 
-    function prirad(){
-        if (playGame){
+function addingScore() {
+    if (playGame) {
         totalScore[activePlayer] = totalScore[activePlayer] + roundScore;
         document.querySelector(".totalScore" + activePlayer).textContent = totalScore[activePlayer];
-        if(totalScore[activePlayer] >= 20){
-            document.querySelector(".player"+ activePlayer).textContent = "VÍTĚZ!!!";
-            document.querySelector(".player"+ activePlayer).style.color = "red";
+        if (totalScore[activePlayer] >= 20) {
+            document.querySelector(".player" + activePlayer).textContent = "VÍTĚZ!!!";
+            document.querySelector(".player" + activePlayer).style.color = "red";
             playGame = false;
             document.querySelector(".btn5").hover.style.backgroundColor = "red";
         }
         else {
-            zmenaHrace();
+            changingPlayer();
         };
-    };   
+    };
 };
 
 
-///////////tlačítko Nová hra
-document.querySelector(".btn7").addEventListener("click",novaHra);
+///////////BUTTON "NOVÁ HRA" - button "new game"
+document.querySelector(".btn7").addEventListener("click", newGame);
 
-function novaHra(){
-    nulovani();
-    totalScore = [0,0];
+function newGame() {
+    zero();
+    totalScore = [0, 0];
     roundScore = 0;
     activePlayer = 0;
     playGame = true;
-    
     document.querySelector(".btn1").classList.add("active");
     document.querySelector(".btn2").classList.remove("active");
     document.querySelector(".player0").textContent = ("HRÁČ 1");
     document.querySelector(".player1").textContent = ("HRÁČ 2");
     document.querySelector(".player1").style.color = "black";
-    document.querySelector(".player0").style.color = "black"; 
-    };
-    
-    
-////////////fce změna aktivního hráče
+    document.querySelector(".player0").style.color = "black";
+};
 
-function zmenaHrace (){
-    if(activePlayer === 0){
+
+////////////function change active player
+function changingPlayer() {
+    if (activePlayer === 0) {
         activePlayer = 1;
     } else {
-        activePlayer=0;
-        };
-        roundScore = 0;
-        document.querySelector(".btn1").classList.toggle("active");
-        document.querySelector(".btn2").classList.toggle("active");
-        document.querySelector(".dice").style.display = "none";
-        document.querySelector(".actualScore0").textContent = 0;
-        document.querySelector(".actualScore1").textContent = 0;
+        activePlayer = 0;
+    };
+    roundScore = 0;
+    document.querySelector(".btn1").classList.toggle("active");
+    document.querySelector(".btn2").classList.toggle("active");
+    document.querySelector(".dice").style.display = "none";
+    document.querySelector(".actualScore0").textContent = 0;
+    document.querySelector(".actualScore1").textContent = 0;
 };
-    
 
 
-////////////fce Vynulování score a odstranění kostky
-
-function nulovani() {
+////////////function zero - null score and delete dice picture
+function zero() {
     document.querySelector(".totalScore0").textContent = 0;
     document.querySelector(".totalScore1").textContent = 0;
     document.querySelector(".actualScore0").textContent = 0;
